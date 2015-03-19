265c53457e2e184332f5f75dc25a6c44938f9638
Fernando Tubio <fernando.tubio@southworks.net>
2015-03-19 17:40:48 -0300

    Implemented getManifest, enableOfflinePage, disableOfflinePage for Windows platform and enhanced error handling.

--- src/windows/HostedWebAppPluginProxy.js
+++ src/windows/HostedWebAppPluginProxy.js
@@ -1,6 +1,9 @@
 ﻿var _manifest;
+var _manifestError;
 var _offlineView;
 var _mainView;
+var _zIndex = 10000;
+var _enableOfflineSupport = true;
 
 // creates a webview to host content
 function configureHost(url, zOrder, display) {
@@ -27,12 +30,13 @@ function configureHost(url, zOrder, display) {
 
 // handle network connectivity change events
 function connectivityEvent(evt) {
-    if (evt.type === 'offline') {
-        _offlineView.style.display = 'block';
-        console.log('The application is currently offline.');
-    } else if (evt.type === 'online') {
-        _offlineView.style.display = 'none';
-        console.log('The application is currently online.');
+    console.log('Received a network connectivity change notification. The device is currently ' + evt.type);
+    if (_enableOfflineSupport) {
+        if (evt.type === 'offline') {
+            _offlineView.style.display = 'block';
+        } else if (evt.type === 'online') {
+            _offlineView.style.display = 'none';
+        }
     }
 }
 
@@ -47,7 +51,7 @@ function configureOfflineSupport(offlinePage) {
         function (err) {
             var message = 'It looks like you are offline. Please reconnect to use this application.';
             var offlinePageTemplate = '<html><body><div style="font-size: x-large; position: absolute; left: 0; top: 0; bottom: 0; right: 0; height: 0; text-align: center; margin: auto">' + message + '</div></body></html>';
-            _offlineView = configureHost(null, 10001, 'none');
+            _offlineView = configureHost(null, _zIndex + 1, 'none');
 
             if (cordova.platformId === 'windows8') {
                 _offlineView.style.backgroundColor = 'white';
@@ -61,43 +65,63 @@ function configureOfflineSupport(offlinePage) {
         });
 }
 
-// loads the W3C manifest file
-function _loadManifestAsync(fileName, successCallback, errorCallback) {
-    var configFile = 'ms-appx:///www/' + fileName;
-    var uri = new Windows.Foundation.Uri(configFile);
-    Windows.Storage.StorageFile.getFileFromApplicationUriAsync(uri).then(
-        function (file) {
-            Windows.Storage.FileIO.readTextAsync(file).then(function (data) {
-                _manifest = JSON.parse(data);
-                if (successCallback) {
-                    successCallback(_manifest);
+module.exports = {
+    // loads the W3C manifest file and parses it
+    loadManifest: function (successCallback, errorCallback, args) {
+        var manifestFileName = (args && args instanceof Array && args.length > 0) ? args[0] : 'manifest.json';
+        var configFile = 'ms-appx:///www/' + manifestFileName;
+        var uri = new Windows.Foundation.Uri(configFile);
+        Windows.Storage.StorageFile.getFileFromApplicationUriAsync(uri).then(
+            function (file) {
+                Windows.Storage.FileIO.readTextAsync(file).then(function (data) {
+                    try {
+                        _manifest = JSON.parse(data);
+                        if (successCallback) {
+                            successCallback(_manifest);
+                        }
+                    } catch (err) {
+                        _manifestError = 'Error parsing manifest file: ' + manifestFileName + ' - ' + err.message;
+                        console.log(_manifestError);
+                    }
+                });
+            },
+            function (err) {
+                _manifestError = 'Error reading manifest file: ' + manifestFileName + ' - ' + err;
+                console.log(_manifestError);
+                if (errorCallback) {
+                    errorCallback(err);
                 }
             });
-        },
-        errorCallback);
-}
+    },
 
-module.exports = {
-    load: function (successCallback, errorCallback, args) {
-        if (args && args.length > 0) {
-            _loadManifestAsync(args[0], successCallback, errorCallback);
+    // returns the currently loaded manifest
+    getManifest: function (successCallback, errorCallback) {
+        if (_manifest) {
+            if (successCallback) {
+                successCallback(_manifest);
+            }
+        } else {
+            if (errorCallback) {
+                errorCallback(new Error(_manifestError));
+            }
         }
-        else if (_manifest) {
-            successCallback(_manifest);
-        }
-        else {
-            errorCallback('Manifest has not been loaded!');
-        }
+    },
+
+    // enables offline page support
+    enableOfflinePage: function () {
+        _enableOfflineSupport = true;
+    },
+
+    // disables offline page support
+    disableOfflinePage: function () {
+        _enableOfflineSupport = false;
     }
 }; // exports
 
 cordova.commandProxy.add('HostedWebApp', module.exports);
 
-_loadManifestAsync('manifest.json',
+module.exports.loadManifest(
     function (manifest) {
-        _mainView = configureHost(manifest.start_url, 10000);
+        _mainView = configureHost(manifest ? manifest.start_url : 'about:blank', _zIndex);
         configureOfflineSupport('offline.html');
-    },
-    function () {
-
     });
